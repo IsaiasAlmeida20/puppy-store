5013fa26abbac3b9e8fbafa0f2c661a285a49fae
Isaias Almeida <isaiasalmeida319@gmail.com>
2023-09-13 11:33:03 -0300

    :sparkles: feat: create Puppy view

--- puppies/views.py
+++ puppies/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Puppy
+from .serializers import PuppySerializer
 
-# Create your views here.
+
+@api_view('GET', 'DELETE', 'PUT')
+def get_delete_update_puppy(request, pk):
+    try:
+        puppy = Puppy.objects.get(pk=pk)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        return Response({})
+
+    elif request.method == 'DELETE':
+        return Response({})
+
+    elif request.method == 'PUT':
+        return Response({})
+
+def get_post_puppies(request):
+
+    if request.method == 'GET':
+        return Response({})
+
+    elif request.method == 'POST':
+        return Response({})
